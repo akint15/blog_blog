2d8d389892f5ccf86b4f87083a0442eeb0586411
akint15 <akint@chicagotechacademy.org>
2013-12-16 13:22:20 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogBlog::Application.config.secret_token = '818e58d19b9b336ae7b3288a2da1c59a32666664984a49bc1a168954ef0e115bf20dccbcddd74ce74c35be394f4a4b88fa99773f6cf491606e4f353caebaeec2'
